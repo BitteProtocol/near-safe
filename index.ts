278762ca6736d00e22b709166f1b24627775ce11
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-06-14 18:03:29 +0200

    Add Near Adapter for Signature (#1)
    This PR adds NearEthAdapter and Signature producing.
    
    Some Encounterd Issues & Resolutions:
    
    Solved (kinda): Conflicting Import Formats in Dependencies
    Solved: Invalid Request Payload for sendUserOp
    Solved: Entry Point Not Supported.
    Solved: Reverted or Out of Gas [Likely Reverted]
    Solved: AA24 signature error
    Solved: Safe Address retrieval after deployment
    Solved: Safe already exists for nonce > 0.
    
    Unresolved/Open Issues (For Future PRs)
    
    - UserOpHash is returned from sendUserOp, not the TxHash. There are endpoints to get the a receipt (containing the txHash), but they are not immediately available... should we sleep or continue requesting until its available? #2
    
    - Gas Fee Fetching is naive (bundler suggests using theirs). #3
    - Integrate Paymaster #4
    
    Bonus: Recovery: AddOwnerWithThreashold at or after Setup.
    
    Co-authored-by: Nicholas Rodrigues Lordello <nick@safe.global>

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-// TODO(nlordell): Should probably be configured correctly with tsconfig.json
-/// <reference lib="dom" />
-
 import {
   getProxyFactoryDeployment,
   getSafeL2SingletonDeployment,
@@ -11,9 +8,10 @@ import {
 } from "@safe-global/safe-modules-deployments";
 import dotenv from "dotenv";
 import { ethers } from "ethers";
+import { NearEthAdapter, MultichainContract } from "near-ca";
 
 dotenv.config();
-const { NEAR_EVM_ADDRESS, SAFE_SALT_NONCE, ERC4337_BUNDLER_URL } = process.env;
+const { SAFE_SALT_NONCE, ERC4337_BUNDLER_URL, ETH_RPC } = process.env;
 
 type DeploymentFunction = (filter?: {
   version: string;
@@ -40,13 +38,25 @@ async function getDeployment(
   );
 }
 
-async function getNearSignature(hash: ethers.BytesLike) {
-  // TODO(bh2smith): do your thing
-  //assert(ethers.recoverAddress(hash, "0x...") === NEAR_EVM_ADDRESS);
-  return ethers.solidityPacked(["uint256", "uint256", "uint8"], [1, 2, 27]);
+async function getNearSignature(
+  adapter: NearEthAdapter,
+  hash: ethers.BytesLike,
+): Promise<`0x${string}`> {
+  const viemHash = typeof hash === "string" ? (hash as `0x${string}`) : hash;
+  // MPC Contract produces two possible signatures.
+  const signatures = await adapter.sign(viemHash);
+  for (const sig of signatures) {
+    if (
+      ethers.recoverAddress(hash, sig).toLocaleLowerCase() ===
+      adapter.address.toLocaleLowerCase()
+    ) {
+      return sig;
+    }
+  }
+  throw new Error("Invalid signature!");
 }
 
-async function sendUserOperation(userOp: unknown, entryPoint: string) {
+async function sendUserOperation(userOp: UserOperation, entryPoint: string) {
   const response = await fetch(ERC4337_BUNDLER_URL!, {
     method: "POST",
     headers: {
@@ -65,25 +75,88 @@ async function sendUserOperation(userOp: unknown, entryPoint: string) {
   }
   const json = JSON.parse(body);
   if (json.error) {
-    throw new Error(json.error.message ?? JSON.stringify(json.error));
+    throw new Error(JSON.stringify(json.error));
+  }
+  // This does not contain a transaction receipt! It is the `userOpHash`
+  return json.result;
+}
+
+async function getUserOpReceipt(userOpHash: string) {
+  const response = await fetch(ERC4337_BUNDLER_URL!, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      jsonrpc: "2.0",
+      method: "eth_getUserOperationReceipt",
+      id: 4337,
+      params: [userOpHash],
+    }),
+  });
+  const body = await response.text();
+  if (!response.ok) {
+    throw new Error(`Failed to send user op ${body}`);
+  }
+  const json = JSON.parse(body);
+  if (json.error) {
+    throw new Error(JSON.stringify(json.error));
   }
   return json.result;
 }
 
+async function getSafeAddressForSetup(
+  contracts: ContractSuite,
+  setup: ethers.BytesLike,
+  saltNonce?: string,
+): Promise<ethers.AddressLike> {
+  // bytes32 salt = keccak256(abi.encodePacked(keccak256(initializer), saltNonce));
+  // cf: https://github.com/safe-global/safe-smart-account/blob/499b17ad0191b575fcadc5cb5b8e3faeae5391ae/contracts/proxies/SafeProxyFactory.sol#L58
+  const salt = ethers.keccak256(
+    ethers.solidityPacked(
+      ["bytes32", "uint256"],
+      [ethers.keccak256(setup), saltNonce || 0],
+    ),
+  );
+
+  // abi.encodePacked(type(SafeProxy).creationCode, uint256(uint160(_singleton)));
+  // cf: https://github.com/safe-global/safe-smart-account/blob/499b17ad0191b575fcadc5cb5b8e3faeae5391ae/contracts/proxies/SafeProxyFactory.sol#L29
+  const initCode = ethers.solidityPacked(
+    ["bytes", "uint256"],
+    [
+      await contracts.proxyFactory.proxyCreationCode(),
+      await contracts.singleton.getAddress(),
+    ],
+  );
+  return ethers.getCreate2Address(
+    await contracts.proxyFactory.getAddress(),
+    salt,
+    ethers.keccak256(initCode),
+  );
+}
+
 async function main() {
-  const provider = new ethers.JsonRpcProvider("https://rpc.sepolia.org");
+  const provider = new ethers.JsonRpcProvider(ETH_RPC);
+  const nearAdapter = await NearEthAdapter.fromConfig({
+    mpcContract: await MultichainContract.fromEnv(),
+  });
+  console.log(
+    `NearEth Adapter: ${nearAdapter.nearAccountId()} <> ${nearAdapter.address}`,
+  );
 
   const safeDeployment = (fn: DeploymentFunction) =>
     getDeployment(fn, { provider, version: "1.4.1" });
   const m4337Deployment = (fn: DeploymentFunction) =>
     getDeployment(fn, { provider, version: "0.3.0" });
+  // Need this first to get entryPoint address
+  const m4337 = await m4337Deployment(getSafe4337ModuleDeployment);
   const contracts = {
     singleton: await safeDeployment(getSafeL2SingletonDeployment),
     proxyFactory: await safeDeployment(getProxyFactoryDeployment),
-    m4337: await m4337Deployment(getSafe4337ModuleDeployment),
+    m4337,
     moduleSetup: await m4337Deployment(getSafeModuleSetupDeployment),
     entryPoint: new ethers.Contract(
-      "0x0000000071727De22E5E9d8BAf0edAc6f37da032",
+      await m4337.SUPPORTED_ENTRYPOINT(),
       [`function getNonce(address, uint192 key) view returns (uint256 nonce)`],
       provider,
     ),
@@ -92,7 +165,7 @@ async function main() {
   const setup = await contracts.singleton.interface.encodeFunctionData(
     "setup",
     [
-      [NEAR_EVM_ADDRESS],
+      [nearAdapter.address],
       1,
       contracts.moduleSetup.target,
       contracts.moduleSetup.interface.encodeFunctionData("enableModules", [
@@ -104,14 +177,16 @@ async function main() {
       ethers.ZeroAddress,
     ],
   );
-  const safeAddress =
-    await contracts.proxyFactory.createProxyWithNonce.staticCall(
-      contracts.singleton,
-      setup,
-      SAFE_SALT_NONCE,
-    );
-
+  const safeAddress = await getSafeAddressForSetup(
+    contracts,
+    setup,
+    SAFE_SALT_NONCE,
+  );
+  console.log("Safe Address:", safeAddress);
   const safeNotDeployed = (await provider.getCode(safeAddress)) === "0x";
+  // TODO(bh2smith) Use Bundler Gas Data Feed:
+  // Error: maxPriorityFeePerGas must be at least 330687958 (current maxPriorityFeePerGas: 328006616)
+  // - use pimlico_getUserOperationGasPrice to get the current gas price
   const { maxPriorityFeePerGas, maxFeePerGas } = await provider.getFeeData();
   if (!maxPriorityFeePerGas || !maxFeePerGas) {
     throw new Error("no gas fee data");
@@ -130,22 +205,24 @@ async function main() {
       : {}),
     // <https://github.com/safe-global/safe-modules/blob/9a18245f546bf2a8ed9bdc2b04aae44f949ec7a0/modules/4337/contracts/Safe4337Module.sol#L172>
     callData: contracts.m4337.interface.encodeFunctionData("executeUserOp", [
-      ethers.ZeroAddress,
-      0,
-      "0x",
+      nearAdapter.address,
+      1n, // 1 wei
+      ethers.hexlify(
+        ethers.toUtf8Bytes("https://github.com/bh2smith/nearly-safe"),
+      ),
       0,
     ]),
     verificationGasLimit: ethers.toBeHex(safeNotDeployed ? 500000 : 100000),
     callGasLimit: ethers.toBeHex(100000),
     preVerificationGas: ethers.toBeHex(100000),
-    maxPriorityFeePerGas: ethers.toBeHex((maxPriorityFeePerGas * 13n) / 10n),
+    maxPriorityFeePerGas: ethers.toBeHex((maxPriorityFeePerGas * 15n) / 10n),
     maxFeePerGas: ethers.toBeHex(maxFeePerGas),
     // TODO(bh2smith): Use paymaster at some point
     //paymaster: paymasterAddress,
     //paymasterGasLimit: ethers.toBeHex(100000),
     //paymasterData: paymasterCallData,
   };
-  console.log(unsignedUserOp);
+  // console.log("Unsigned UserOp", unsignedUserOp);
 
   const packGas = (hi: ethers.BigNumberish, lo: ethers.BigNumberish) =>
     ethers.solidityPacked(["uint128", "uint128"], [hi, lo]);
@@ -168,15 +245,49 @@ async function main() {
     paymasterAndData: "0x",
     signature: ethers.solidityPacked(["uint48", "uint48"], [0, 0]),
   });
-  console.log(safeOpHash);
-
-  const signature = await getNearSignature(safeOpHash);
-  console.log(
-    await sendUserOperation(
-      { ...unsignedUserOp, signature },
-      await contracts.entryPoint.getAddress(),
-    ),
+  console.log("Safe Op Hash", safeOpHash);
+  console.log("Signing with Near...");
+  const signature = ethers.solidityPacked(
+    ["uint48", "uint48", "bytes"],
+    [0, 0, await getNearSignature(nearAdapter, safeOpHash)],
   );
+  const userOpHash = await sendUserOperation(
+    { ...unsignedUserOp, signature },
+    await contracts.entryPoint.getAddress(),
+  );
+  console.log("UserOp Hash", userOpHash);
+  // TODO(bh2smith) this is returning null because we are requesting it too soon!
+  // Maybe better to `eth_getUserOperationByHash` (although this also returns null).
+  const userOpReceipt = await getUserOpReceipt(userOpHash);
+  console.log("userOp Receipt", userOpReceipt);
+}
+
+/**
+ * Supported Representation of UserOperation for EntryPoint v0.7
+ */
+interface UserOperation {
+  sender: ethers.AddressLike;
+  nonce: string;
+  factory?: ethers.AddressLike;
+  factoryData?: ethers.BytesLike;
+  callData: string;
+  verificationGasLimit: string;
+  callGasLimit: string;
+  preVerificationGas: string;
+  maxPriorityFeePerGas: string;
+  maxFeePerGas: string;
+  signature: string;
+}
+
+/**
+ * All contracts used in account creation & execution
+ */
+interface ContractSuite {
+  singleton: ethers.Contract;
+  proxyFactory: ethers.Contract;
+  m4337: ethers.Contract;
+  moduleSetup: ethers.Contract;
+  entryPoint: ethers.Contract;
 }
 
 main().catch((err) => {
