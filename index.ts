a0d5e47d8aab38ad8f56c777d1486bc8d9c2d506
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-06-26 12:35:48 +0200

    non-functional sdk attempt

--- index.ts
+++ index.ts
@@ -9,6 +9,12 @@ import {
 import dotenv from "dotenv";
 import { ethers } from "ethers";
 import { NearEthAdapter, MultichainContract } from "near-ca";
+import { UserOperation } from "@safe-global/safe-core-sdk-types";
+import {
+  PimlicoFeeEstimator,
+  Safe4337Pack,
+  UserOperationReceipt,
+} from "@safe-global/relay-kit";
 
 dotenv.config();
 const { SAFE_SALT_NONCE, ERC4337_BUNDLER_URL, ETH_RPC } = process.env;
@@ -57,6 +63,15 @@ async function getNearSignature(
 }
 
 async function sendUserOperation(userOp: UserOperation, entryPoint: string) {
+  const hexifiedUserOp = {
+    ...userOp,
+    verificationGasLimit: ethers.toBeHex(userOp.verificationGasLimit),
+    callGasLimit: ethers.toBeHex(userOp.callGasLimit),
+    preVerificationGas: ethers.toBeHex(userOp.preVerificationGas),
+    maxPriorityFeePerGas: ethers.toBeHex(userOp.maxPriorityFeePerGas),
+    maxFeePerGas: ethers.toBeHex(userOp.maxFeePerGas),
+  };
+  console.log(hexifiedUserOp);
   const response = await fetch(ERC4337_BUNDLER_URL!, {
     method: "POST",
     headers: {
@@ -66,7 +81,7 @@ async function sendUserOperation(userOp: UserOperation, entryPoint: string) {
       jsonrpc: "2.0",
       method: "eth_sendUserOperation",
       id: 4337,
-      params: [userOp, entryPoint],
+      params: [hexifiedUserOp, entryPoint],
     }),
   });
   const body = await response.text();
@@ -81,29 +96,29 @@ async function sendUserOperation(userOp: UserOperation, entryPoint: string) {
   return json.result;
 }
 
-async function getUserOpReceipt(userOpHash: string) {
-  const response = await fetch(ERC4337_BUNDLER_URL!, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      jsonrpc: "2.0",
-      method: "eth_getUserOperationReceipt",
-      id: 4337,
-      params: [userOpHash],
-    }),
-  });
-  const body = await response.text();
-  if (!response.ok) {
-    throw new Error(`Failed to send user op ${body}`);
-  }
-  const json = JSON.parse(body);
-  if (json.error) {
-    throw new Error(JSON.stringify(json.error));
-  }
-  return json.result;
-}
+// async function getUserOpReceipt(userOpHash: string) {
+//   const response = await fetch(ERC4337_BUNDLER_URL!, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       jsonrpc: "2.0",
+//       method: "eth_getUserOperationReceipt",
+//       id: 4337,
+//       params: [userOpHash],
+//     }),
+//   });
+//   const body = await response.text();
+//   if (!response.ok) {
+//     throw new Error(`Failed to send user op ${body}`);
+//   }
+//   const json = JSON.parse(body);
+//   if (json.error) {
+//     throw new Error(JSON.stringify(json.error));
+//   }
+//   return json.result;
+// }
 
 async function getSafeAddressForSetup(
   contracts: ContractSuite,
@@ -162,122 +177,186 @@ async function main() {
     ),
   };
 
-  const setup = await contracts.singleton.interface.encodeFunctionData(
-    "setup",
-    [
-      [nearAdapter.address],
-      1,
-      contracts.moduleSetup.target,
-      contracts.moduleSetup.interface.encodeFunctionData("enableModules", [
-        [contracts.m4337.target],
-      ]),
-      contracts.m4337.target,
-      ethers.ZeroAddress,
-      0,
-      ethers.ZeroAddress,
-    ],
-  );
-  const safeAddress = await getSafeAddressForSetup(
-    contracts,
-    setup,
-    SAFE_SALT_NONCE,
-  );
+  const safe4337Pack = await Safe4337Pack.init({
+    provider: ETH_RPC!,
+    rpcUrl: ETH_RPC!,
+    bundlerUrl: ERC4337_BUNDLER_URL!,
+    options: {
+      owners: [nearAdapter.address],
+      threshold: 1,
+      saltNonce: "0",
+    },
+  });
+  safe4337Pack.protocolKit.getAddress();
+  const safeAddress = await safe4337Pack.protocolKit.getAddress();
   console.log("Safe Address:", safeAddress);
+  return;
+  // const setup = await contracts.singleton.interface.encodeFunctionData(
+  //   "setup",
+  //   [
+  //     [nearAdapter.address],
+  //     1,
+  //     contracts.moduleSetup.target,
+  //     contracts.moduleSetup.interface.encodeFunctionData("enableModules", [
+  //       [contracts.m4337.target],
+  //     ]),
+  //     contracts.m4337.target,
+  //     ethers.ZeroAddress,
+  //     0,
+  //     ethers.ZeroAddress,
+  //   ],
+  // );
+  // const safeAddress = await getSafeAddressForSetup(
+  //   contracts,
+  //   setup,
+  //   SAFE_SALT_NONCE,
+  // );
+  const preUserOp = await safe4337Pack.createTransaction({
+    transactions: [
+      {
+        to: nearAdapter.address,
+        value: "1", // 1 wei
+        data: ethers.hexlify(
+          ethers.toUtf8Bytes("https://github.com/bh2smith/nearly-safe"),
+        ),
+      },
+    ],
+  });
+  const estimator = new PimlicoFeeEstimator();
+  // test doesn't align with interface declaration:
+  // test: https://github.com/safe-global/safe-core-sdk/blob/8896940929eb23b154e0e6615226f1f7c004c7fb/packages/relay-kit/src/packs/safe-4337/estimators/PimlicoFeeEstimator.test.ts#L29-L33
+  // interface (only uses bundlerUrl): https://github.com/safe-global/safe-core-sdk/blob/8896940929eb23b154e0e6615226f1f7c004c7fb/packages/relay-kit/src/packs/safe-4337/estimators/PimlicoFeeEstimator.ts#L13
+  const sponsoredGasEstimation = await estimator.setupEstimation({
+    bundlerUrl: ERC4337_BUNDLER_URL!,
+    userOperation: preUserOp.toUserOperation(),
+    entryPoint: await contracts.entryPoint.getAddress(),
+  });
+
+  // This didn't modify the object! Or at least IDE can't tell.
+  // unsignedUserOp.addEstimations(sponsoredGasEstimation);
+  // Fee estimation only contains: { maxFeePerGas: '0x208ff4', maxPriorityFeePerGas: '0x150ea0' }
   const safeNotDeployed = (await provider.getCode(safeAddress)) === "0x";
-  // TODO(bh2smith) Use Bundler Gas Data Feed:
-  // Error: maxPriorityFeePerGas must be at least 330687958 (current maxPriorityFeePerGas: 328006616)
-  // - use pimlico_getUserOperationGasPrice to get the current gas price
-  const { maxPriorityFeePerGas, maxFeePerGas } = await provider.getFeeData();
-  if (!maxPriorityFeePerGas || !maxFeePerGas) {
-    throw new Error("no gas fee data");
-  }
-  const unsignedUserOp = {
-    sender: safeAddress,
-    nonce: ethers.toBeHex(await contracts.entryPoint.getNonce(safeAddress, 0)),
-    ...(safeNotDeployed
-      ? {
-          factory: contracts.proxyFactory.target,
-          factoryData: contracts.proxyFactory.interface.encodeFunctionData(
-            "createProxyWithNonce",
-            [contracts.singleton.target, setup, SAFE_SALT_NONCE],
-          ),
-        }
-      : {}),
-    // <https://github.com/safe-global/safe-modules/blob/9a18245f546bf2a8ed9bdc2b04aae44f949ec7a0/modules/4337/contracts/Safe4337Module.sol#L172>
-    callData: contracts.m4337.interface.encodeFunctionData("executeUserOp", [
-      nearAdapter.address,
-      1n, // 1 wei
-      ethers.hexlify(
-        ethers.toUtf8Bytes("https://github.com/bh2smith/nearly-safe"),
-      ),
-      0,
-    ]),
-    verificationGasLimit: ethers.toBeHex(safeNotDeployed ? 500000 : 100000),
-    callGasLimit: ethers.toBeHex(100000),
-    preVerificationGas: ethers.toBeHex(100000),
-    maxPriorityFeePerGas: ethers.toBeHex((maxPriorityFeePerGas * 15n) / 10n),
-    maxFeePerGas: ethers.toBeHex(maxFeePerGas),
-    // TODO(bh2smith): Use paymaster at some point
-    //paymaster: paymasterAddress,
-    //paymasterGasLimit: ethers.toBeHex(100000),
-    //paymasterData: paymasterCallData,
+  const userOp = {
+    ...preUserOp.toUserOperation(),
+    ...sponsoredGasEstimation,
+    verificationGasLimit: safeNotDeployed ? 500000n : 100000n,
+    callGasLimit: 100000n,
+    preVerificationGas: 100000n,
   };
+
+  console.log("Fee Estimation", sponsoredGasEstimation);
+  // const safeNotDeployed = (await provider.getCode(safeAddress)) === "0x";
+  // // TODO(bh2smith) Use Bundler Gas Data Feed:
+  // // Error: maxPriorityFeePerGas must be at least 330687958 (current maxPriorityFeePerGas: 328006616)
+  // // - use pimlico_getUserOperationGasPrice to get the current gas price
+  // const { maxPriorityFeePerGas, maxFeePerGas } = await provider.getFeeData();
+  // if (!maxPriorityFeePerGas || !maxFeePerGas) {
+  //   throw new Error("no gas fee data");
+  // }
+
+  // const unsignedUserOp = {
+  //   sender: safeAddress,
+  //   nonce: ethers.toBeHex(await contracts.entryPoint.getNonce(safeAddress, 0)),
+  //   ...(safeNotDeployed
+  //     ? {
+  //         factory: contracts.proxyFactory.target,
+  //         factoryData: contracts.proxyFactory.interface.encodeFunctionData(
+  //           "createProxyWithNonce",
+  //           [contracts.singleton.target, setup, SAFE_SALT_NONCE],
+  //         ),
+  //       }
+  //     : {}),
+  //   // <https://github.com/safe-global/safe-modules/blob/9a18245f546bf2a8ed9bdc2b04aae44f949ec7a0/modules/4337/contracts/Safe4337Module.sol#L172>
+  //   callData: contracts.m4337.interface.encodeFunctionData("executeUserOp", [
+  //     nearAdapter.address,
+  //     1n, // 1 wei
+  //     ethers.hexlify(
+  //       ethers.toUtf8Bytes("https://github.com/bh2smith/nearly-safe"),
+  //     ),
+  //     0,
+  //   ]),
+  //   verificationGasLimit: ethers.toBeHex(safeNotDeployed ? 500000 : 100000),
+  //   callGasLimit: ethers.toBeHex(100000),
+  //   preVerificationGas: ethers.toBeHex(100000),
+  //   maxPriorityFeePerGas: ethers.toBeHex((maxPriorityFeePerGas * 15n) / 10n),
+  //   maxFeePerGas: ethers.toBeHex(maxFeePerGas),
+  //   // TODO(bh2smith): Use paymaster at some point
+  //   //paymaster: paymasterAddress,
+  //   //paymasterGasLimit: ethers.toBeHex(100000),
+  //   //paymasterData: paymasterCallData,
+  // };
   // console.log("Unsigned UserOp", unsignedUserOp);
 
   const packGas = (hi: ethers.BigNumberish, lo: ethers.BigNumberish) =>
     ethers.solidityPacked(["uint128", "uint128"], [hi, lo]);
-  const safeOpHash = await contracts.m4337.getOperationHash({
-    ...unsignedUserOp,
-    initCode: unsignedUserOp.factory
-      ? ethers.solidityPacked(
-          ["address", "bytes"],
-          [unsignedUserOp.factory, unsignedUserOp.factoryData],
-        )
-      : "0x",
-    accountGasLimits: packGas(
-      unsignedUserOp.verificationGasLimit,
-      unsignedUserOp.callGasLimit,
-    ),
-    gasFees: packGas(
-      unsignedUserOp.maxPriorityFeePerGas,
-      unsignedUserOp.maxFeePerGas,
-    ),
+  const safeOpHash = await m4337.getOperationHash({
+    ...userOp,
+    accountGasLimits: packGas(userOp.verificationGasLimit, userOp.callGasLimit),
+    gasFees: packGas(userOp.maxPriorityFeePerGas, userOp.maxFeePerGas),
     paymasterAndData: "0x",
     signature: ethers.solidityPacked(["uint48", "uint48"], [0, 0]),
   });
+  // const safeOpHash = await contracts.m4337.getOperationHash({
+  //   ...unsignedUserOp,
+  //   initCode: unsignedUserOp.factory
+  //     ? ethers.solidityPacked(
+  //         ["address", "bytes"],
+  //         [unsignedUserOp.factory, unsignedUserOp.factoryData],
+  //       )
+  //     : "0x",
+  //   accountGasLimits: packGas(
+  //     unsignedUserOp.verificationGasLimit,
+  //     unsignedUserOp.callGasLimit,
+  //   ),
+  //   gasFees: packGas(
+  //     unsignedUserOp.maxPriorityFeePerGas,
+  //     unsignedUserOp.maxFeePerGas,
+  //   ),
+  //   paymasterAndData: "0x",
+  //   signature: ethers.solidityPacked(["uint48", "uint48"], [0, 0]),
+  // });
   console.log("Safe Op Hash", safeOpHash);
   console.log("Signing with Near...");
   const signature = ethers.solidityPacked(
     ["uint48", "uint48", "bytes"],
     [0, 0, await getNearSignature(nearAdapter, safeOpHash)],
   );
+
+  // Not sure if we can use the sdk here because we didn't "sign" the Safe Operation, but rather the UserOperation.
   const userOpHash = await sendUserOperation(
-    { ...unsignedUserOp, signature },
+    { ...userOp, signature },
     await contracts.entryPoint.getAddress(),
   );
   console.log("UserOp Hash", userOpHash);
   // TODO(bh2smith) this is returning null because we are requesting it too soon!
   // Maybe better to `eth_getUserOperationByHash` (although this also returns null).
-  const userOpReceipt = await getUserOpReceipt(userOpHash);
-  console.log("userOp Receipt", userOpReceipt);
+  // const userOpReceipt = await getUserOpReceipt(userOpHash);
+  // console.log("userOp Receipt", userOpReceipt);
+  let userOpReceipt: UserOperationReceipt | null = null;
+
+  while (!userOpReceipt) {
+    // Wait 2 seconds before checking the status again
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    userOpReceipt = await safe4337Pack.getUserOperationReceipt(userOpHash);
+  }
 }
 
-/**
- * Supported Representation of UserOperation for EntryPoint v0.7
- */
-interface UserOperation {
-  sender: ethers.AddressLike;
-  nonce: string;
-  factory?: ethers.AddressLike;
-  factoryData?: ethers.BytesLike;
-  callData: string;
-  verificationGasLimit: string;
-  callGasLimit: string;
-  preVerificationGas: string;
-  maxPriorityFeePerGas: string;
-  maxFeePerGas: string;
-  signature: string;
-}
+// /**
+//  * Supported Representation of UserOperation for EntryPoint v0.7
+//  */
+// interface UserOperation {
+//   sender: ethers.AddressLike;
+//   nonce: string;
+//   factory?: ethers.AddressLike;
+//   factoryData?: ethers.BytesLike;
+//   callData: string;
+//   verificationGasLimit: string;
+//   callGasLimit: string;
+//   preVerificationGas: string;
+//   maxPriorityFeePerGas: string;
+//   maxFeePerGas: string;
+//   signature: string;
+// }
 
 /**
  * All contracts used in account creation & execution
