cf9e6104b039e3e7e19fb8efb6f5588ce53c8e17
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-09-17 14:24:53 +0200

    Viem Bundler (#54)

--- src/lib/bundler.ts
+++ src/lib/bundler.ts
@@ -1,6 +1,15 @@
-// TODO: Ethers dependency is only for Generic HTTP Provider
-import { ethers } from "ethers";
-import { toHex } from "viem";
+import {
+  Address,
+  createPublicClient,
+  Hash,
+  http,
+  PublicClient,
+  rpcSchema,
+  toHex,
+  Transport,
+  RpcError,
+  HttpRequestError,
+} from "viem";
 
 import {
   GasPrices,
@@ -15,19 +24,43 @@ function bundlerUrl(chainId: number, apikey: string): string {
   return `https://api.pimlico.io/v2/${chainId}/rpc?apikey=${apikey}`;
 }
 
+type BundlerRpcSchema = [
+  {
+    Method: "pm_sponsorUserOperation";
+    Parameters: [UnsignedUserOperation, Address];
+    ReturnType: PaymasterData;
+  },
+  {
+    Method: "eth_sendUserOperation";
+    Parameters: [UserOperation, Address];
+    ReturnType: Hash;
+  },
+  {
+    Method: "pimlico_getUserOperationGasPrice";
+    Parameters: [];
+    ReturnType: GasPrices;
+  },
+  {
+    Method: "eth_getUserOperationReceipt";
+    Parameters: [Hash];
+    ReturnType: UserOperationReceipt | null;
+  },
+];
+
 export class Erc4337Bundler {
-  provider: ethers.JsonRpcProvider;
-  entryPointAddress: string;
+  client: PublicClient<Transport, undefined, undefined, BundlerRpcSchema>;
+  entryPointAddress: Address;
   apiKey: string;
   chainId: number;
 
-  constructor(entryPointAddress: string, apiKey: string, chainId: number) {
+  constructor(entryPointAddress: Address, apiKey: string, chainId: number) {
     this.entryPointAddress = entryPointAddress;
     this.apiKey = apiKey;
     this.chainId = chainId;
-    this.provider = new ethers.JsonRpcProvider(
-      bundlerUrl(chainId, this.apiKey)
-    );
+    this.client = createPublicClient({
+      transport: http(bundlerUrl(chainId, this.apiKey)),
+      rpcSchema: rpcSchema<BundlerRpcSchema>(),
+    });
   }
 
   async getPaymasterData(
@@ -38,39 +71,39 @@ export class Erc4337Bundler {
     // TODO: Keep this option out of the bundler
     if (usePaymaster) {
       console.log("Requesting paymaster data...");
-      const data = this.provider.send("pm_sponsorUserOperation", [
-        { ...rawUserOp, signature: PLACEHOLDER_SIG },
-        this.entryPointAddress,
-      ]);
-      return data;
+      return handleRequest<PaymasterData>(() =>
+        this.client.request({
+          method: "pm_sponsorUserOperation",
+          params: [
+            { ...rawUserOp, signature: PLACEHOLDER_SIG },
+            this.entryPointAddress,
+          ],
+        })
+      );
     }
     return defaultPaymasterData(safeNotDeployed);
   }
 
-  async sendUserOperation(userOp: UserOperation): Promise<string> {
-    try {
-      const userOpHash = await this.provider.send("eth_sendUserOperation", [
-        userOp,
-        this.entryPointAddress,
-      ]);
-      return userOpHash;
-    } catch (err: unknown) {
-      const error = (err as ethers.JsonRpcError).error;
-      throw new Error(`Failed to send user op with: ${error.message}`);
-    }
+  async sendUserOperation(userOp: UserOperation): Promise<Hash> {
+    return handleRequest<Hash>(() =>
+      this.client.request({
+        method: "eth_sendUserOperation",
+        params: [userOp, this.entryPointAddress],
+      })
+    );
+    // throw new Error(`Failed to send user op with: ${error.message}`);
   }
 
   async getGasPrice(): Promise<GasPrices> {
-    return this.provider.send("pimlico_getUserOperationGasPrice", []);
+    return handleRequest<GasPrices>(() =>
+      this.client.request({
+        method: "pimlico_getUserOperationGasPrice",
+        params: [],
+      })
+    );
   }
 
-  async _getUserOpReceiptInner(
-    userOpHash: string
-  ): Promise<UserOperationReceipt | null> {
-    return this.provider.send("eth_getUserOperationReceipt", [userOpHash]);
-  }
-
-  async getUserOpReceipt(userOpHash: string): Promise<UserOperationReceipt> {
+  async getUserOpReceipt(userOpHash: Hash): Promise<UserOperationReceipt> {
     let userOpReceipt: UserOperationReceipt | null = null;
     while (!userOpReceipt) {
       // Wait 2 seconds before checking the status again
@@ -78,6 +111,38 @@ export class Erc4337Bundler {
       userOpReceipt = await this._getUserOpReceiptInner(userOpHash);
     }
     return userOpReceipt;
+  }
+
+  private async _getUserOpReceiptInner(
+    userOpHash: Hash
+  ): Promise<UserOperationReceipt | null> {
+    return handleRequest<UserOperationReceipt | null>(() =>
+      this.client.request({
+        method: "eth_getUserOperationReceipt",
+        params: [userOpHash],
+      })
+    );
+  }
+}
+
+async function handleRequest<T>(clientMethod: () => Promise<T>): Promise<T> {
+  try {
+    return await clientMethod();
+  } catch (error) {
+    if (error instanceof HttpRequestError) {
+      if (error.status === 401) {
+        throw new Error("Unauthorized request. Please check your API key.");
+      } else {
+        console.error(
+          `Request failed with status ${error.status}: ${error.message}`
+        );
+      }
+    } else if (error instanceof RpcError) {
+      throw new Error(`Failed to send user op with: ${error.message}`);
+    }
+    throw new Error(
+      `Unexpected error ${error instanceof Error ? error.message : String(error)}`
+    );
   }
 }
 
