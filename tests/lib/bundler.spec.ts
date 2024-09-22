adfa8b5383c2d756f391bf36f60f7af3fb97f9d4
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-09-22 14:11:50 +0200

    exclude api key from pimlico request errors

--- tests/lib/bundler.spec.ts
+++ tests/lib/bundler.spec.ts
@@ -1,15 +1,27 @@
-import { Erc4337Bundler } from "../../src/lib/bundler";
+import { Erc4337Bundler, stripApiKey } from "../../src/lib/bundler";
+
 describe("Safe Pack", () => {
   const entryPoint = "0x0000000071727De22E5E9d8BAf0edAc6f37da032";
-
   it("Unauthorized Requests Failure", async () => {
     const unauthorizedBundler = new Erc4337Bundler(
       entryPoint,
-      "invalidAPI key",
+      "invalid thirty six character API key",
       11155111
     );
     await expect(() => unauthorizedBundler.getGasPrice()).rejects.toThrow(
-      "Unauthorized request. Please check your API key."
+      "Unauthorized request. Please check your Pimlico API key."
+    );
+  });
+
+  it.only("Strips API Key from error message", () => {
+    const apiKey = "any-thirty-six-character-long-string";
+    const message = (x: string): string => `Unexpected Error
+    URL: https://api.pimlico.io/v2/11155111/rpc?apikey=${x}
+    Additional Error Context`;
+    expect(stripApiKey(new Error(message(apiKey)))).toEqual(message("***"));
+
+    expect(stripApiKey(new Error(message("TopSecret")))).toEqual(
+      message("***")
     );
   });
 });
