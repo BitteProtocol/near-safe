a63948f0df8311579584201b2aa246e4524a8a1d
Benjamin Smith <bh2smith@users.noreply.github.com>
2024-11-14 09:37:56 +0100

    SignRequest: Determine Broadcast Target (#110)

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ export * from "./constants";
 export * from "./decode";
 export * from "./lib/safe-message";
 
+// TODO: Improve re-exports...
 export {
   Network,
   BaseTx,
@@ -15,4 +16,5 @@ export {
   signatureFromTxHash,
   requestRouter as mpcRequestRouter,
   EthTransactionParams,
+  isRlpHex,
 } from "near-ca";
